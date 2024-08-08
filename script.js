0008b58e887dee6503d6c1cc42d720ee9681a6ce
Ytikare <ytikare2@tutanota.com>
2024-08-08 18:10:46 +0300

    Add reset grip button functionality

--- script.js
+++ script.js
@@ -1,9 +1,22 @@
 const containerDiv = document.querySelector('.container');
+const resetButton = document.querySelector('.button-div > button');
+
 
 const initialCount = 16;
 
 fillGrid(initialCount);
 
+
+resetButton.addEventListener('click', e => {
+    let str = prompt('Enter an integer between 1 and 100');
+
+    if (str && isNaN(str) == false) {
+        let count = Number(str);
+        if (Number.isInteger(count)) {
+            fillGrid(count);
+        }
+    }
+})
 
 function fillGrid(count) {
     containerDiv.innerHTML = '';
