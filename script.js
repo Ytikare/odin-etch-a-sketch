691053748c5fc44ee7eefb87eb74139e5fbf9ff6
Ytikare <ytikare2@tutanota.com>
2024-08-08 15:35:42 +0300

    Add color change on mouse hover

--- script.js
+++ script.js
@@ -3,5 +3,10 @@ const containerDiv = document.querySelector('.container');
 for (let i = 0; i < 16; i++) {
     const divElement = document.createElement('div');
     divElement.classList.add('pad');
+    
+    divElement.addEventListener('mouseover', e => {
+        e.target.classList.add('fill-color');
+    })
+    
     containerDiv.appendChild(divElement);
 }
