e54c17cca32997728f8bdfdba63c15ba62f547a3
Ytikare <ytikare2@tutanota.com>
2024-08-08 17:31:37 +0300

    Wrap filling process into a function

--- script.js
+++ script.js
@@ -1,12 +1,26 @@
 const containerDiv = document.querySelector('.container');
 
-for (let i = 0; i < 16; i++) {
-    const divElement = document.createElement('div');
-    divElement.classList.add('pad');
-    
-    divElement.addEventListener('mouseover', e => {
-        e.target.classList.add('fill-color');
-    })
-    
-    containerDiv.appendChild(divElement);
+const initialCount = 16;
+
+fillGrid(initialCount);
+
+
+function fillGrid(count) {
+    containerDiv.innerHTML = '';
+
+    for (let i = 0; i < count; i++) {
+        for (let j = 0; j < count; j++) {
+
+            const divElement = document.createElement('div');
+            divElement.classList.add('pad');
+
+            divElement.style.flex = `1 0 calc(100% / ${count + 1})`;
+
+            divElement.addEventListener('mouseover', e => {
+                e.target.classList.add('fill-color');
+            })
+            containerDiv.appendChild(divElement);
+        }
+    }
+
 }
